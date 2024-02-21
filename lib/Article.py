e83a9420ef8ad7231d8d2b28f9ec2168ea5e6b95
Wilson Gachanja <wilson.gachanja@student.moringaschool.com>
2024-02-21 10:09:23 +0300

    Updated code and tested on Main.py

--- lib/Article.py
+++ lib/Article.py
@@ -1,22 +1,27 @@
 class Article:
-    _all_articles = []
+    all_articles = []
 
     def __init__(self, author, magazine, title):
         self._author = author
         self._magazine = magazine
         self._title = title
-        Article._all_articles.append(self)
+        self.__class__.all_articles.append(self)
+        magazine.published_articles.append(self)
+        author.authored_articles.append(self)
 
+    @property
     def title(self):
         return self._title
 
+    @property
     def author(self):
         return self._author
 
+    @property
     def magazine(self):
         return self._magazine
 
     @classmethod
     def all(cls):
-        return cls._all_articles
+        return cls.all_articles
     pass
--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,41 +1,50 @@
-from lib.Article import Article
-
-
 class Magazine:
-    _all_magazines = []
+    all_magazines = []
 
     def __init__(self, name, category):
         self._name = name
         self._category = category
-        self._articles = []
-        Magazine._all_magazines.append(self)
+        self.additional_info = {}
+        self.published_articles = []
+        self.__class__.all_magazines.append(self)
 
+    @property
     def name(self):
         return self._name
 
+    @property
     def category(self):
         return self._category
 
-    def all():
-        return Magazine._all_magazines
-
-    def contributors(self):
-        return list(set(article.author() for article in self._articles))
-
-    def add_article(self, author, title):
-        new_article = Article(author, self, title)
-        self._articles.append(new_article)
-        return new_article
+    @classmethod
+    def all(cls):
+        return cls.all_magazines
 
     @classmethod
     def find_by_name(cls, name):
-        return next((magazine for magazine in cls._all_magazines if magazine.name() == name), None)
+        return next((magazine for magazine in cls.all_magazines if magazine.name == name), None)
 
     @classmethod
-    def article_titles(cls):
-        return [article.title() for magazine in cls._all_magazines for article in magazine._articles]
+    def article_titles(cls, magazine_name):
+        magazine = cls.find_by_name(magazine_name)
+        if magazine:
+            return [article.title for article in magazine.published_articles]
+        else:
+            return []
 
-    @classmethod
-    def contributing_authors(cls):
-        return [author for magazine in cls._all_magazines for author in magazine.contributors() if len(author.articles()) > 2]
+    def contributing_authors(self):
+        authors_count = {}
+        for article in self.published_articles:
+            author = article.author
+            authors_count[author] = authors_count.get(author, 0) + 1
+        return [author for author, count in authors_count.items() if count > 2]
+
+    def update_info(self, key, value):
+        self.additional_info[key] = value
+
+    def contributors(self):
+        return list(set(article.author for article in self.published_articles))
+
+    def __str__(self):
+        return f"Magazine: {self._name}, Category: {self._category}, Additional Info: {self.additional_info}"
     pass
